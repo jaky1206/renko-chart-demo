bb60830ad3811535d2c9f9dafc8c13bd4744e1e6
Sayed Rokibul Hossain <jaky1206@yandex.com>
2024-08-28 20:50:08 +0700

    renko from database and file

--- renko_using_plotly_from_sql_server.py
+++ renko_using_plotly_from_sql_server.py
@@ -5,20 +5,162 @@ import pandas as pd
 # NEW: Modified for Renko plotting >> Start
 import plotly.graph_objects as go
 import dash
+import pyodbc
 
 from dash import Dash, dcc, html, Input, Output, State
+from datetime import datetime
+
+# make a static class
+class DATASOURCE():
+    FILE = 1
+    SQL_SERVER = 2
 
 # Constants
 BRICK_SIZE = 10  # Define the brick size
-SHOW_LEGENDS = False  # Set to True to show the legend
+SHOW_LEGENDS = True  # Set to True to show the legend
+DATA_SROUCE = DATASOURCE.SQL_SERVER
+COLUMN_LIST = ['Time_Start', 'Renko_Open', 'Renko_Close', 'Volume', 'Moving_Average', 'Median']
+
+# SQL Server connection details
+MSSQL_SERVER_ADDRESS = 'localhost'  # Your SQL Server name or address
+MSSQL_DATABASE = 'TradingDB'  # Your database name
+MSSQL_USER_NAME = 'sa'  # Your SQL Server username
+MSSQL_DATABASE_PASSWORD = 'orion123@'  # Your SQL Server password
+MSSQL_ODBC_DRIVER = '{ODBC Driver 18 for SQL Server}'  # ODBC driver
 
 # Initialize Dash app
 app = Dash(__name__)
 # NEW: Modified for Renko plotting >> End
 
-dataframes = []
 file_paths = []
 current_index = 0
+
+
+# NEW: Modified for Renko plotting >> Start
+def get_sql_server_connection_string():
+    return (
+            f'DRIVER={MSSQL_ODBC_DRIVER};'
+            f'SERVER={MSSQL_SERVER_ADDRESS};'
+            f'DATABASE={MSSQL_DATABASE};'
+            f'UID={MSSQL_USER_NAME};'
+            f'PWD={MSSQL_DATABASE_PASSWORD};'
+            'TrustServerCertificate=yes;'
+        )
+
+
+def execute_query(query):
+    """
+    Connects to SQL Server, executes the given query, and returns the results.
+
+    Parameters:
+    - query: SQL query string to be executed
+
+    Returns:
+    - results: List of tuples containing the query results
+    """
+    cnxn = None
+    cursor = None
+
+    try:
+        # Create a connection string
+        connection_string = get_sql_server_connection_string()
+
+        # Connect to SQL Server
+        cnxn = pyodbc.connect(connection_string)
+        
+        # Create a cursor object
+        cursor = cnxn.cursor()
+
+        # Execute the provided SQL query
+        cursor.execute(query)
+        
+        # Fetch data
+        results = cursor.fetchall()
+        
+        return results
+
+    except pyodbc.Error as e:
+        print(f"Error: {e}")
+        return None
+
+    finally:
+        # Clean up and close the cursor and connection
+        if cursor:
+            cursor.close()
+        if cnxn:
+            cnxn.close()
+
+def get_list_from_database():
+    # Define the query
+    query = """
+    SELECT DISTINCT
+        [WeekStartDate],
+        [WeekEndDate],
+        [WeekNo]
+    FROM [TradingDB].[dbo].[NQ_Weekly_Data]
+    """
+
+    # Execute the query and fetch data
+    results = execute_query(query)
+
+    # Print results if data was fetched successfully
+    if results is not None:
+        # create an array of strings
+        result = []
+        for row in results:
+            #print('Week No: ',row[2], ' From: ', row[0], ' To: ', row[1])
+            result.append('Week No: ' + str(row[2]) + ' From: ' + str(row[0]) + ' To: ' + str(row[1]))
+
+        return result
+    else:
+        # reutrn None
+        return None
+    
+def extract_week_info(week_str):
+    # Define a regular expression pattern to match the week information
+    pattern = r"Week No: (\d+) From: (\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}) To: (\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2})"
+    
+    # Search for the pattern in the input string
+    match = re.search(pattern, week_str)
+    
+    if match:
+        week_no = int(match.group(1))
+        start_str = match.group(2)
+        end_str = match.group(3)
+        
+        # Parse the date strings into datetime objects
+        start_date = datetime.strptime(start_str, "%Y-%m-%d %H:%M:%S")
+        end_date = datetime.strptime(end_str, "%Y-%m-%d %H:%M:%S")
+        
+        return week_no, start_date, end_date
+    else:
+        # raise ValueError("The input string does not match the expected format")
+        return None, None, None
+
+def get_dataframe_from_sql_server(file_path):
+    # read week week start and week end from the csv file
+    week_no, week_start_date, week_end_date = extract_week_info(file_path)
+    # Define the query
+    query = f"""
+    SELECT
+        [Time_Start],
+        [Renko_Open],
+        [Renko_Close],
+        [Volume],
+        [Indicator_1] AS [Moving_Average],
+        [Indicator_2] AS [Median]
+    FROM [TradingDB].[dbo].[NQ_Weekly_Data]
+    WHERE [WeekStartDate] = '{week_start_date}'
+      AND [WeekEndDate] = '{week_end_date}'
+      AND [WeekNo] = {week_no}
+    """
+
+    connection_string = get_sql_server_connection_string()
+    
+    cnxn = pyodbc.connect(connection_string)
+
+    return pd.read_sql(query, cnxn)
+# NEW: Modified for Renko plotting >> End
 
 def change_working_directory():
     script_directory = os.path.dirname(os.path.abspath(__file__))
@@ -44,19 +186,23 @@ def get_file_paths(directory):
     sorted_files = sorted(all_files, key=sort_key)
     return sorted_files
 
-def load_data(file_paths):
-    # NEW: Modified for Renko plotting >> Start
-    dataframes = []
-    # NEW: Modified for Renko plotting >> End
-    for path in file_paths:
-        df = pd.read_csv(path, usecols=['Time_Start', 'Renko_Open', 'Renko_Close', 'Volume', 'Moving_Average', 'Median'])
-        dataframes.append(df)
-    return dataframes
-
 # NEW: Modified for Renko plotting >> Start
+def load_data(file_path):
+    if DATA_SROUCE == DATASOURCE.SQL_SERVER:
+        df = get_dataframe_from_sql_server(file_path)
+    else:
+        # Load a single DataFrame from the specified file path
+        df = pd.read_csv(file_path, usecols=COLUMN_LIST)
+    return df
+
 def plot_data(index):
-    df = dataframes[index]
-    file_name = os.path.basename(file_paths[index])
+    file_path = file_paths[index]
+    df = load_data(file_path)  # Load data on demand
+    
+    if DATA_SROUCE == DATASOURCE.SQL_SERVER:
+        file_name = file_path
+    else:
+        file_name = os.path.basename(file_path)
     
     fig = go.Figure()
 
@@ -74,7 +220,7 @@ def plot_data(index):
     for i in range(len(df)):
         open_price = df["Renko_Open"].iloc[i]
         close_price = df["Renko_Close"].iloc[i]
-        difference = close_price - open_price
+        difference = pd.to_numeric(close_price) - pd.to_numeric(open_price)
         
         # Determine the color of the brick
         color = 'green' if close_price >= open_price else 'red'
@@ -85,30 +231,35 @@ def plot_data(index):
                 current_y_position += brick_size
             elif color == 'red':
                 current_y_position -= brick_size
-        
+
+        # Convert close_price to numeric type
+        close_price_numeric = pd.to_numeric(close_price)
+
         while abs(difference) >= brick_size:
-            y_start = current_y_position
+            y_start = float(current_y_position)
+            brick_size = float(BRICK_SIZE)  # Define the brick size
             y_end = y_start + (brick_size if difference > 0 else -brick_size)
-            
+
             colors.append(color)
             x_positions.append(df["Time_Start"].iloc[i])
             y_starts.append(min(y_start, y_end))
             y_ends.append(max(y_start, y_end))
-            
+
             current_y_position = y_end  # Update the y-position for the next brick
-            difference = close_price - current_y_position
-        
-        # Draw the remaining part of the brick, if any
-        if abs(difference) > 0:
-            y_start = current_y_position
-            y_end = close_price
-            
-            colors.append(color)
-            x_positions.append(df["Time_Start"].iloc[i])
-            y_starts.append(min(y_start, y_end))
-            y_ends.append(max(y_start, y_end))
-            
-            current_y_position = y_end  # Update the y-position for the next brick
+            difference = close_price_numeric - current_y_position
+
+            # Draw the remaining part of the brick, if any
+            if abs(difference) > 0:
+                y_start = current_y_position
+                y_end = close_price_numeric
+
+                colors.append(color)
+                x_positions.append(df["Time_Start"].iloc[i])
+                y_starts.append(min(y_start, y_end))
+                y_ends.append(max(y_start, y_end))
+
+                current_y_position = y_end  # Update the y-position for the next brick
+                difference = 0
         
         prev_row_renko_color = color
         x_position += 1  # Move to the next x position for the next time interval
@@ -124,53 +275,52 @@ def plot_data(index):
         ),
         width=1,
         name="Renko Bricks",
-        showlegend=SHOW_LEGENDS
     ))
 
     # Plot Moving Average and Median
     fig.add_trace(go.Scatter(
         x=df["Time_Start"],
         y=df["Moving_Average"],
-        mode='lines',
+        mode='lines',  # Ensure it's lines-only
         line=dict(color='blue', width=2),
         name='Moving Average',
-        showlegend=SHOW_LEGENDS
     ))
 
     fig.add_trace(go.Scatter(
         x=df["Time_Start"],
         y=df["Median"],
-        mode='lines',
+        mode='lines',  # Ensure it's lines-only
         line=dict(color='orange', width=2),
         name='Median',
-        showlegend=SHOW_LEGENDS
     ))
 
     fig.update_layout(
-        xaxis_title="Time Start",
-        yaxis_title="Values",
-        yaxis=dict(
-            tickformat=',',  # Ensures thousands separator is displayed
-        ),
-        legend=dict(
+    xaxis_title="Time Start",
+    yaxis_title="",
+    showlegend=SHOW_LEGENDS,  # Disable the legend
+    legend=dict(
             orientation="h",  # Horizontal legend
             yanchor="top",
             y=1.2,
             xanchor="left",
             x=0,
-            # bgcolor='rgba(255, 255, 255, 0)',  # Optional: Transparent background for the legend
         ),
-        xaxis_tickangle=-45,
-        autosize=True,
-        title={
-            'text': f'Renko Chart of {file_name}',
-            'x': 0.5,
-            'xanchor': 'center',
-            'y': 0.95,
-            'yanchor': 'top',
-            'font': {'size': 15}
-        },
-    )
+    margin=dict(l=0, r=0, t=0, b=0),  # Remove margins
+    xaxis_tickangle=-45,
+    autosize=True,
+    title={
+        'text': f'Renko Chart of {file_name}',
+        'x': 0.5,
+        'xanchor': 'center',
+        'y': 0.98,  # Adjust this value to move the title higher
+        'yanchor': 'bottom',
+        'font': {'size': 13}  # Adjust font size as needed
+    },
+    yaxis=dict(
+            tickformat=',',  
+            tickmode='auto'  
+        )
+)
 
     return fig
 
@@ -180,12 +330,13 @@ def plot_data(index):
 
 change_working_directory()
 
-# Get the file paths from the "DATA" subdirectory
-file_paths = get_file_paths(r'./data/custom-format/renko-parsed')
-# Load all the data
-dataframes = load_data(file_paths)
-
 # NEW: Modified for Renko plotting >> Start
+
+# Get the file paths from the "DATA" subdirectory
+if DATA_SROUCE == DATASOURCE.SQL_SERVER:
+    file_paths = get_list_from_database()
+else:
+    file_paths = get_file_paths(r'./data/custom-format/renko-parsed')
 
 # Define Dash layout with CSS for centering and resizing
 app.layout = html.Div([
